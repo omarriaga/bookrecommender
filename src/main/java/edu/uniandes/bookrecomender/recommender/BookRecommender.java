dd80a25872cf0305cbf58e022d129aee54acdf1d
oscar <omarrc9123@gmail.com>
2017-02-27 21:15:15 -0500

    adding recommender handler class

---
+++ src/main/java/edu/uniandes/bookrecomender/recommender/BookRecommender.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uniandes.bookrecomender.recommender;
+
+
+import org.apache.mahout.cf.taste.model.JDBCDataModel;
+
+/**
+ *
+ * @author oscar
+ */
+public class BookRecommender {
+    
+    public void init(){
+        //JDBCDataModel model = new 
+    }
+           
+    
+}
